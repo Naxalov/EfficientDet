31e5f0b46d1d9667192fcb8641d0350446e4bdef
ZARIF ubuntu <uzbzarif@gmail.com>
2021-09-23 17:01:21 +0900

    fix

--- automl-docker/run.sh
+++ automl-docker/run.sh
@@ -1,2 +1,3 @@
-docker run --rm --runtime=nvidia -v $(pwd)/automl/efficientdet:'/github' -it efficient bash
+# docker run --rm --runtime=nvidia -v $(pwd)/automl/efficientdet:'/github' -it efficient bash
+docker run --rm --runtime=nvidia -it efficient bash
 
