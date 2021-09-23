a70aa7f446280ca7d9e78267aa764e7fac536aac
ZARIF ubuntu <uzbzarif@gmail.com>
2021-09-23 17:14:57 +0900

    workspace

--- automl-docker/run.sh
+++ automl-docker/run.sh
@@ -1,3 +1,3 @@
-# docker run --rm --runtime=nvidia -v $(pwd)/automl/efficientdet:'/github' -it efficient bash
-docker run --rm --runtime=nvidia -it efficient bash
+docker run --rm --runtime=nvidia -v $(pwd)/workspace/:'/workspace' -it efficient bash
+# docker run --rm --runtime=nvidia -it efficient bash
 
